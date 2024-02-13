7dc432eafcf56c322713f3f6b9250f0d448d4063
687edf53 <sm3257698@gmail.com>
2024-02-13 14:27:26 +0200

    some improvments

--- JS/translate.js
+++ JS/translate.js
@@ -2,30 +2,27 @@
 const navLinks = document.querySelectorAll('.nav-links li a');
 const LangBtn = document.querySelector('.special-btn');
 
-const navLangObj = {
-  ar: {
-    btn: 'الانجليزية',
-    home: 'الرئيسية',
-    about: 'عنا',
-    contact: 'تواصل معنا'
-  }
-};
-
 LangBtn.addEventListener('click', () => {
   if (LangBtn.textContent.trim() === 'arabic') {
     LangBtn.textContent = 'انجليزية';
 
-    navLinks[0].textContent = navLangObj.ar.home;
-    navLinks[1].textContent = navLangObj.ar.about;
-    navLinks[2].textContent = navLangObj.ar.contact;
+    navLinks[0].textContent = 'الرئيسية';
+    navLinks[1].textContent = 'عنا';
+    navLinks[2].textContent = 'تواصل معنا';
+
+    document.documentElement.dir = 'rtl'
+    document.documentElement.lang = 'ar'
   } else {
     LangBtn.textContent = 'arabic';
-    navLinks[0].textContent = Object.keys(navLangObj.ar)[1];
-    navLinks[1].textContent = Object.keys(navLangObj.ar)[2];
-    navLinks[2].textContent = Object.keys(navLangObj.ar)[3];
+    navLinks[0].textContent = 'Home';
+    navLinks[1].textContent = 'About';
+    navLinks[2].textContent = 'Contact';
+
+    document.documentElement.dir = 'ltr'
+    document.documentElement.lang = 'en'
   }
 });
-// End Navbar
+// End Navbarlang
 
 // Start Header
 const headerName = document.querySelectorAll('header .my_name .hidden-msg > div');
@@ -40,23 +37,30 @@ LangBtn.addEventListener('click', () => {
     headerName[1].textContent = 'mohamed hossam';
     headerName[0].parentElement.classList.remove('arabic')
     headerName[2].textContent = 'How are you?';
+
     headerJob[0].textContent = 'frontend'
     headerJob[1].textContent = 'developer'
     headerJob[0].parentElement.classList.remove('arabic')
+
     headerButtons[0].textContent = 'See My Projects';
     headerButtons[1].textContent = 'See My Tasks';
+
     headerP.textContent = 'As a seasoned frontend developer, I specialize in transforming concepts into visually stunning and user-friendly websites. With an eye for detail and a flair for creativity'
   } else {
     headerTitle.textContent = '!مرحبا بكم';
+
     headerName[0].textContent = 'محمد حسام';
     headerName[1].textContent = 'محمد حسام';
     headerName[2].textContent = 'كيف حالك؟';
     headerName[0].parentElement.classList.add('arabic')
+
     headerJob[0].textContent = 'مطور'
     headerJob[1].textContent = 'مواقع'
     headerJob[0].parentElement.classList.add('arabic')
+
     headerButtons[0].textContent = 'شاهد المشاريع';
     headerButtons[1].textContent = 'شاهد المهام';
+
     headerP.textContent = 'باعتباري مطورًا متمرسًا للواجهة الأمامية، فأنا متخصص في تحويل المفاهيم إلى مواقع ويب مذهلة بصريًا وسهلة الاستخدام. مع الاهتمام بالتفاصيل وذوق الإبداع'
   }
 });
