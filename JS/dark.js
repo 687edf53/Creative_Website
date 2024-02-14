bb20a03c85eb82093e1041e210544134eec03a01
687edf53 <sm3257698@gmail.com>
2024-02-14 20:21:34 +0200

    make clean code

--- JS/dark.js
+++ JS/dark.js
@@ -1,27 +1,35 @@
-// Start Global Rules
-const rootStyle = document.documentElement.style;
-// End Global Rules
+// ---- Start Variables ----
 
-// Start Navbar
+// Global Rules
+const rootStyle = document.documentElement.style;
+
+// Navbar
 const btnMode = document.querySelector('.mode-btn');
 const btnModeIcon = document.querySelector('.mode-btn i');
 
-btnMode.addEventListener('click', () => {
+// Header
+const headerTitle = document.querySelector('header h2');
 
+// ---- End Variables ----
+
+// ---- Start Color Mode ----
+btnMode.addEventListener('click', () => {
   if (btnModeIcon.className == 'fa-solid fa-moon') {
+
     rootStyle.setProperty('--main-color', 'white');
     rootStyle.setProperty('--light-color', 'black');
+
     btnModeIcon.className = 'fa-solid fa-sun';
+
     headerTitle.classList.add('text-shine');
   } else {
+
     rootStyle.setProperty('--main-color', 'black');
     rootStyle.setProperty('--light-color', 'white');
+
     btnModeIcon.className = 'fa-solid fa-moon';
+
     headerTitle.classList.remove('text-shine');
   }
 });
-// End Navbar
-
-// Start Header
-const headerTitle = document.querySelector('header h2');
-// End Header
+// ---- End Color Mode ----
--- JS/translate.js
+++ JS/translate.js
@@ -1,73 +1,75 @@
-// Start Navbar
+// Start Variables
+
+// Navbar
 const navLinks = document.querySelectorAll('.nav-links li a');
 const LangBtn = document.querySelector('.special-btn');
 
-LangBtn.addEventListener('click', () => {
-  if (LangBtn.textContent.trim() === 'arabic') {
-    LangBtn.textContent = 'انجليزية';
-
-    navLinks[0].textContent = 'الرئيسية';
-    navLinks[1].textContent = 'عنا';
-    navLinks[2].textContent = 'تواصل معنا';
-
-    document.documentElement.dir = 'rtl'
-    document.documentElement.lang = 'ar'
-  } else {
-    LangBtn.textContent = 'arabic';
-    navLinks[0].textContent = 'Home';
-    navLinks[1].textContent = 'About';
-    navLinks[2].textContent = 'Contact';
-
-    document.documentElement.dir = 'ltr'
-    document.documentElement.lang = 'en'
-  }
-});
-// End Navbarlang
-
-// Start Header
+// Header
 const headerName = document.querySelectorAll('header .my_name .hidden-msg > div');
 const headerButtons = document.querySelectorAll('header .half-btns button');
 const headerJob = document.querySelectorAll('header .my_job > span')
 const headerP = document.querySelector('header p')
 
+// End Variables
+
+// Start Translation
 LangBtn.addEventListener('click', () => {
   if (LangBtn.textContent.trim() === 'arabic') {
+
+    // Nav
+    LangBtn.textContent = 'انجليزية';
+    
+    navLinks[0].textContent = 'الرئيسية';
+    navLinks[1].textContent = 'عنا';
+    navLinks[2].textContent = 'تواصل معنا';
+
+    // Header
+    headerTitle.textContent = '!مرحبا بكم';
+
+    headerName[0].textContent = 'محمد حسام';
+    headerName[1].textContent = 'محمد حسام';
+    headerName[2].textContent = 'كيف حالك؟';
+
+    headerJob[0].textContent = 'مطور'
+    headerJob[1].textContent = 'مواقع'
+
+    headerButtons[0].textContent = 'شاهد المشاريع';
+    headerButtons[1].textContent = 'شاهد المهام';
+    
+    headerP.textContent = 'باعتباري مطورًا متمرسًا للواجهة الأمامية، فأنا متخصص في تحويل المفاهيم إلى مواقع ويب مذهلة بصريًا وسهلة الاستخدام. مع الاهتمام بالتفاصيل وذوق الإبداع'
+
+    document.body.classList.add('arabic')
+
+    document.documentElement.dir = 'rtl'
+    document.documentElement.lang = 'ar'
+
+  } else {
+
+    // Nav
+    LangBtn.textContent = 'arabic';
+    navLinks[0].textContent = 'Home';
+    navLinks[1].textContent = 'About';
+    navLinks[2].textContent = 'Contact';
+
+    // Header
     headerTitle.textContent = 'Hello there!';
-    headerTitle.classList.remove('arabic')
 
     headerName[0].textContent = 'mohamed hossam';
     headerName[1].textContent = 'mohamed hossam';
-    headerName[0].parentElement.classList.remove('arabic')
     headerName[2].textContent = 'How are you?';
 
     headerJob[0].textContent = 'frontend'
     headerJob[1].textContent = 'developer'
-    headerJob[0].parentElement.classList.remove('arabic')
     
     headerButtons[0].textContent = 'See My Projects';
     headerButtons[1].textContent = 'See My Tasks';
-    headerButtons[0].parentElement.classList.remove('arabic')
     
     headerP.textContent = 'As a seasoned frontend developer, I specialize in transforming concepts into visually stunning and user-friendly websites. With an eye for detail and a flair for creativity'
-  } else {
-    headerTitle.textContent = '!مرحبا بكم';
-    headerTitle.classList.add('arabic')
-    
-    headerName[0].textContent = 'محمد حسام';
-    headerName[1].textContent = 'محمد حسام';
-    headerName[2].textContent = 'كيف حالك؟';
-    headerName[0].parentElement.classList.add('arabic')
-    
 
-    headerJob[0].textContent = 'مطور'
-    headerJob[1].textContent = 'مواقع'
-    headerJob[0].parentElement.classList.add('arabic')
-    
-    headerButtons[0].textContent = 'شاهد المشاريع';
-    headerButtons[1].textContent = 'شاهد المهام';
-    headerButtons[0].parentElement.classList.add('arabic')
-    
-    headerP.textContent = 'باعتباري مطورًا متمرسًا للواجهة الأمامية، فأنا متخصص في تحويل المفاهيم إلى مواقع ويب مذهلة بصريًا وسهلة الاستخدام. مع الاهتمام بالتفاصيل وذوق الإبداع'
+    document.body.classList.remove('arabic')
+
+    document.documentElement.dir = 'ltr'
+    document.documentElement.lang = 'en'
   }
 });
-// End Header
+// End Translation
